959b11c9f8624ee77617338f73eba650bf0e8bbd
Le Hong ngan <hongnganmayman@gmail.com>
2022-02-26 22:54:35 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
+
 $(document).ready(function(){
-    $('.col').slick({
-        infinite: true,
+         $('.sub-menu').parent('li').addClass('has-child');
+    });
+
+var counter = 1;
+    setInterval(function(){
+       document.getElementById('radio' + counter).checked = true;
+       counter++;
+       if (counter > 4) {
+        counter = 1;
+       } 
+    }, 3000);
+
+$(document).ready(function(){
+    $('.row').slick({
+     
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 2,
+        infinite: true,
+        arrow:true,
+        autoplaySpeed: 2000,
+        autoplay: true,
+            prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+            nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"       
       });
   });
