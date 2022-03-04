c3b820509cc65c69c92c07bcd6407a00a31bfa18
Le Hong ngan <86295743+hongngan45@users.noreply.github.com>
2022-03-04 20:45:31 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ var counter = 1;
 $(document).ready(function(){
     $('.row').slick({
      
-        slidesToShow: 4,
+        slidesToShow: 3,
         slidesToScroll: 2,
         infinite: true,
         arrow:true,
@@ -29,7 +29,7 @@ $(document).ready(function(){
   $(document).ready(function(){
     $('.productss').slick({
      
-        slidesToShow: 3,
+        slidesToShow: 4,
         slidesToScroll: 2,
         infinite: true,
         arrow:true,
