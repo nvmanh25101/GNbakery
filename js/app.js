5182308f4b35b37dd7c1225ce424503ddf83d781
Le Hong ngan <86295743+hongngan45@users.noreply.github.com>
2022-03-04 20:42:24 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
   $(document).ready(function(){
     $('.productss').slick({
      
-        slidesToShow: 4,
+        slidesToShow: 3,
         slidesToScroll: 2,
         infinite: true,
         arrow:true,
