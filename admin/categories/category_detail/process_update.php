fa8435acb707f612d72bf32612a166e6f6055876
Manh <nvmanh25101@gmail.com>
2022-03-10 18:34:44 +0700

    update(admin): products-update

--- admin/categories/category_detail/process_update.php
+++ admin/categories/category_detail/process_update.php
@@ -15,37 +15,10 @@ if(empty($_POST['name'])) {
 }
 
 $name = $_POST['name'];
-$image_old = $_POST['image_old'];
-$image_new = $_FILES['image_new'];
-
-if($image_new['size'] > 0) {
-    $folder = '../../assets/images/categories/';
-    $file_extension = explode('.', $image_new['name'])[1]; //explode: cắt chuỗi = dấu . thành mảng lấy vị trí thứ 1
-    $file_name = 'category_' . time() . '.' . $file_extension; // tránh trùng ảnh
-    $path_file = $folder . $file_name;
-    $file_type = array("jpg", "jpeg", "png");
-
-    if(!in_array("$file_extension", $file_type)) {
-        $_SESSION['error'] = 'Chỉ cho phép file dạng .JPG, .PNG, .JPEG'; 
-        header("location:form_update.php?id=$id");
-        exit();
-    }
-
-    if ($image_new["size"] > 500000) {
-        $_SESSION['error'] = 'File của bạn quá lớn!'; 
-        header("location:form_update.php?id=$id");
-        exit();
-    }
-
-    move_uploaded_file($image_new['tmp_name'], $path_file);
-}
-else {
-    $file_name = $image_old;
-}
 
 require_once '../../database/connect.php';
 
-$sql = "update categories
+$sql = "update category_detail
 set name = ?,
 image = ?
 where id = '$id'";
